8ec6cbcb3a9f7641946886bb878b8820a5beb644
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2019-10-04 12:14:48 +0430

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {
+    ObjectID
+} = require("mongodb");
 
 var {
     mongoose
@@ -35,6 +38,33 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET/todos/1234324
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // valid id using isValid
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(400).send();
+        }
+
+        res.send({
+            todo
+        });
+
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+
+
+
 });
 
 module.exports = {
